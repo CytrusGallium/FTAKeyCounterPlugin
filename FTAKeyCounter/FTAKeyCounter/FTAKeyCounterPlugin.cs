c440449ba0372037f93fb323e1096dbae02dc8f0
Abdeldjelil BABAHAMED <8852906+CytrusGallium@users.noreply.github.com>
2022-07-26 12:07:35 +0100

    Initial Commit

---
+++ FTAKeyCounter/FTAKeyCounter/FTAKeyCounterPlugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FocusTimeAccumulator.Features.Plugins;
+using FocusTimeAccumulator.Features.Pool;
+
+namespace FTAKeyCounter
+{
+    public class FTAKeyCounterPlugin : Plugin
+    {
+        public override void OnStart()
+        {
+            Console.WriteLine("+----------------------------+");
+            Console.WriteLine("| Key Counter Plugin Enabled |");
+            Console.WriteLine("+----------------------------+");
+
+            MouseClickCounter.StartMouseClickCounter();
+        }
+
+        public override void OnTick()
+        {
+            //Console.WriteLine("FTAKeyCounterPlugin : Tick");
+        }
+
+        public override void OnProcessChanged(Process process, string prevName, string prevTitle, string appName, string appTitle)
+        {
+            Console.WriteLine("!! PROCESS CHANGE EVENT !!");
+            //Console.WriteLine(prevName);
+            //Console.WriteLine(prevTitle);
+            //Console.WriteLine(appName);
+            //Console.WriteLine(appTitle);
+        }
+
+        public override void ModifyPoolAppUpdate(PoolApp.AppSpan packet)
+        {
+            Console.WriteLine("!! MODIFICATION EVENT !!");
+            //Console.WriteLine(MouseClickCounter.ConsumeLeftClickCount());
+            //Console.WriteLine(MouseClickCounter.ConsumeRightClickCount());
+            
+            int lmb = 0;
+            if (packet.stats.ContainsKey("lmb"))
+                lmb = packet.stats["lmb"].value;
+
+            int rmb = 0;
+            if (packet.stats.ContainsKey("rmb"))
+                lmb = packet.stats["rmb"].value;
+
+            packet.AddStat("lmb", lmb + MouseClickCounter.ConsumeLeftClickCount());
+            packet.AddStat("rmb", rmb + MouseClickCounter.ConsumeRightClickCount());
+        }
+    }
+}
---
+++ FTAKeyCounter/FTAKeyCounter/MouseClickCounter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace FTAKeyCounter
+{
+    internal class MouseClickCounter
+    {
+        public static bool IsCounting => clickCountThread.IsAlive;
+
+        // for different Virtual-Keys view list:
+        // https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+        const Int32 LEFT_MOUSE_BUTTON = 0x01;
+        const Int32 RIGHT_MOUSE_BUTTON = 0x02;
+
+        private static bool waitingLeftUp = false;
+        private static bool waitingRightUp = false;
+        private static int leftClickCounter = 0;
+        private static int rightClickCounter = 0;
+        private static Thread clickCountThread;
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int VirtualKeyPressed);
+
+        public static void UpdateClickCounters()
+        {
+            if (GetAsyncKeyState(LEFT_MOUSE_BUTTON) == 0)
+            {
+                if (waitingLeftUp)
+                {
+                    //Console.WriteLine("Left Click !");
+                    leftClickCounter++;
+                    waitingLeftUp = false;
+                }
+            }
+            else
+            {
+                waitingLeftUp = true;
+            }
+
+            if (GetAsyncKeyState(RIGHT_MOUSE_BUTTON) == 0)
+            {
+                if (waitingRightUp)
+                {
+                    //Console.WriteLine("Right Click !");
+                    rightClickCounter++;
+                    waitingRightUp = false;
+                }
+            }
+            else
+            {
+                waitingRightUp = true;
+            }
+        }
+
+        /// <summary>
+        /// Starts a thread that counts left and right mouse clicks.
+        /// </summary>
+        public static void StartMouseClickCounter()
+        {
+            clickCountThread = new Thread(ClickCountThread);
+            clickCountThread.Start();
+        }
+
+        private static void ClickCountThread()
+        {
+            while(true)
+            {
+                UpdateClickCounters();
+            }
+        }
+
+        /// <summary>
+        /// Returns how many times the left mouse button was clicked and resets the counter to zero.
+        /// </summary>
+        public static int ConsumeLeftClickCount()
+        {
+            int tmp = leftClickCounter;
+            leftClickCounter = 0;
+            return tmp;
+        }
+
+        /// <summary>
+        /// Returns how many times the right mouse button was clicked and resets the counter to zero.
+        /// </summary>
+        public static int ConsumeRightClickCount()
+        {
+            int tmp = rightClickCounter;
+            rightClickCounter = 0;
+            return tmp;
+        }
+    }
+}
